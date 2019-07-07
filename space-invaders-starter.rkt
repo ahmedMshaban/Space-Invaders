07aa2a67fe0f2311cbd114eb5f76b7c207b893bf
ahmedMshaban <ahmed.ms.elias@gmail.com>
2019-07-07 15:00:14 +0300

    wish list entriesfor big-bang handlers

--- space-invaders-starter.rkt
+++ space-invaders-starter.rkt
@@ -6,9 +6,7 @@
 
 ;; Space Invaders
 
-;; Constants:
-- WIDTH
-- HEIGHT
+
 ;; Constants:
 
 (define WIDTH  300)
@@ -46,7 +44,7 @@
 ;; Data Definitions:
 
 (define-struct game (invaders missiles tank))
-;; Game is (make-game  (listof Invader) (listof Missile) Tank)
+;; Game is (make-game  ListOfInvader ListOfMissile Tank)
 ;; interp. the current state of a space invaders game
 ;;         with the current invaders, missiles and tank position
 
@@ -90,6 +88,31 @@
   (... (invader-x invader) (invader-y invader) (invader-dx invader)))
 
 
+
+;; ListOfInvader is one of:
+;; - empty
+;; - (cons Invader ListOfInvader)
+;; interp. a list of Invaders
+
+(define LOI1 empty)
+(define LOI2 (cons (make-invader 150 100 12) (cons (make-invader 150 HEIGHT -10) empty)))
+
+#;
+(define (fn-for-loi loi)
+  (cond [(empty? loi) (...)]
+        [else
+         (... (fn-for-invader (first loi))
+              (fn-for-loi (rest loi)))]))
+
+;; Template Rules used:
+;; - one-of: 2 cases
+;; - atomic distinct: empty
+;; - compound: (cons Invader ListOfInvader)
+;; - reference: (first loi) is Invader
+;; - self reference: (rest loi) is ListOfInvader
+
+
+
 (define-struct missile (x y))
 ;; Missile is (make-missile Number Number)
 ;; interp. the missile's location is x y in screen coordinates
@@ -103,9 +126,77 @@
   (... (missile-x m) (missile-y m)))
 
 
+;; ListOfMissile is one of:
+;; - empty
+;; - (cons Missile ListOfMissile)
+;; interp. a list of Missiles
+
+(define LOM1 empty)
+(define LOM2 (cons (make-missile 150 300) (cons (make-missile (invader-x I1) (+ (invader-y I1) 10)) empty)))
+
+#;
+(define (fn-for-lom lom)
+  (cond [(empty? lom) (...)]
+        [else
+         (... (fn-for-missile (first lom))
+              (fn-for-lom (rest lom)))]))
+
+;; Template Rules used:
+;; - one-of: 2 cases
+;; - atomic distinct: empty
+;; - compound: (cons Missile ListOfMissile)
+;; - reference: (first lom) is Missile
+;; - self reference: (rest lom) is ListOfMissile
+
+
 
 (define G0 (make-game empty empty T0))
 (define G1 (make-game empty empty T1))
 (define G2 (make-game (list I1) (list M1) T1))
 (define G3 (make-game (list I1 I2) (list M1 M2) T1))
+
+
+;; =================
+;; Functions:
+
+;; Game -> Game
+;; start the world with (main G0)
+;; 
+(define (main g)
+  (big-bang g                        ; Game
+    (on-tick   advance-game)         ; Game -> Game
+    (to-draw    render-game)         ; Game -> Image
+    (stop-when    stop-game)         ; Game -> Boolean
+    (on-key      handle-key)))       ; Game KeyEvent -> Game
+
+;; Game -> Game
+;; produce the next game
+;; !!!
+(define (advance-game g) g) ;stub
+
+
+
+;; Game -> Image
+;; render the game
+;; !!!
+(define (render-game g) BACKGROUND) ;stub
+
+
+
+;; Game -> Boolean
+;; When Invader reaches the bottom of the screen, the game is over. 
+;; !!!
+(define (stop-game g) false)
+
+
+
+;; Game KeyEvent -> Game
+;; Hanlde the left key, right key, and space bar key:
+;;           - left: move the tank to left at a constant speed
+;;           - right: move the tank to right at a constant speed
+;;           - space bar: fire missiles straight up from the tank current position
+;; !!!
+(define (handle-key g ke) g) ;stub
+
+
 
